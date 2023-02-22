4ac676d337aa66f11b71470e5ef7e8a6d8610540
hanu02 <hanimule33@gmail.com>
2023-02-22 04:56:37 -0800

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -4,16 +4,16 @@
   *Return : Always 0.
  */
 void print_alphabet_x10(void);
-{
-char c;
-int i = 0;
+  {
+    char c;
+    int i = 0;
  while (i <= 9)
-{
+ {
 	for (c = 'a'; c <= 'z'; c++)
 	{
 	putchar (c);
 	putchar('\n');
 	i++;
 	}
-}
-}
+ } 
+ }
