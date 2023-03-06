e01d75fb1083a73a98bef0f7e658d0f7500b4e56
hanu02 <hanimule33@gmail.com>
2023-03-06 03:55:23 -0800

    6-abs.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,21 +2,20 @@
 #include "main.h"
 /**
  * print_sigh - a function that sigh numbers
- * @n : numbers
- * return : 1 if n is grater than zero, 0 if n is zero
- -1 if n is less tan zero
+ * @n : accepts numbers
+ * Return : 1 if n is grater than zero, 0 if n is zero , -1 if n is less tan zero
  */
 int print_sign(int n)
 {
 if (n > 0)
-{
+{i
 _putchar('+');
-return(1);
+return (1);
 }
 else if (n == 0)
 {
 _putchar('0');
-return(0);
+return (0);
 }
 else
 {
---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * _abs - function that computes absolute value of an integer
+ * return : The absolute value of an integer
+ * @n : theinteger 
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+		return (n);
+	else
+		return (-n);
+}
